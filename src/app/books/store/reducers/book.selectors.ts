05c548cba21219f8142040d4e9dcdfc37729bbe1
Matan Sar-Shalom <smatan84@gmail.com>
2018-07-29 15:29:16 +0300

    fix loaded state leftovers

--- src/app/books/store/reducers/book.selectors.ts
+++ src/app/books/store/reducers/book.selectors.ts
@@ -1,7 +1,6 @@
 import {BooksState} from './book.reducers';
 import {createSelector} from '@ngrx/store';
 
-const getLoaded = (state: BooksState) => state.loaded;
 const getLoading = (state: BooksState) => state.loading;
 const getBooks = (state: BooksState) => state.books;
 
@@ -10,11 +9,6 @@ export const getBooksState = (state: any) => state.books;
 export const getAllBooksSelector = createSelector(
   getBooksState,
   getBooks
-);
-
-export const getBooksLoadedSelector = createSelector(
-  getBooksState,
-  getLoaded
 );
 
 export const getBooksLoadingSelector = createSelector(
